dc5cb19bb8fcae2f2d6051aa9b8cab461d7c3b50
Ahmed Soudy <ahmed.soudy30@yahoo.com>
2025-02-12 16:03:44 +0200

    starting of the chat format

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from auctions.models import Auction
+from django.conf import settings
 
-# Create your models here.
+
+class ChatMessage(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.message[:20]}'
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from auctions.models import Auction
+from .models import ChatMessage
 
 # function-based view
-def chat(request):
-    return render(request, 'chat.html')
+
+def chat(request, auction_id):
+    auction = get_object_or_404(Auction, id=auction_id)
+    messages = ChatMessage.objects.filter(auction=auction).order_by('timestamp')
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        if message:
+            ChatMessage.objects.create(auction=auction, user=request.user, message=message)
+
+    return render(request, 'chat.html', {'auction': auction, 'messages': messages})
