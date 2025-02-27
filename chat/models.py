f3e72e110503bd74f3f744313ffe4668d67f3f2e
Ahmed Soudy <ahmed.soudy30@yahoo.com>
2025-02-27 16:45:21 +0200

    edit the chat

--- chat/models.py
+++ chat/models.py
@@ -7,8 +7,15 @@ from django.conf import settings
 class ChatMessage(models.Model):
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    message = models.TextField()
+    message = models.TextField(max_length=400)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username}: {self.message[:20]}'
+        return f'{self.user.username}: {self.message}'
+    
+    class Meta:
+        ordering = ['-timestamp']
+    
+    
+
+    
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from auctions.models import Auction
 from .models import ChatMessage
 
@@ -9,10 +9,12 @@ from .models import ChatMessage
 def chat(request, auction_id):
     auction = get_object_or_404(Auction, id=auction_id)
     messages = ChatMessage.objects.filter(auction=auction).order_by('timestamp')
-
+    
     if request.method == 'POST':
         message = request.POST.get('message')
         if message:
+            print("The current message: ",message)
             ChatMessage.objects.create(auction=auction, user=request.user, message=message)
+            return redirect('chat', auction_id=auction.id)  # Redirect to prevent form resubmission
 
     return render(request, 'chat.html', {'auction': auction, 'messages': messages})
